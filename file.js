ed0bcfa9f33570b9a67eea19c0140b6d4efbe092
Hunter Fabrizzio <hfabrizzio@ucvts.org>
2018-11-07 10:45:44 -0500

    start

---
+++ file.js
@@ -0,0 +1,12 @@
+function one() {
+  console.log("Hello, World!")
+}
+
+function two() {
+  alert("Hello, World!")
+}
+
+function sayHello() {
+  Var p=document"index.html".getElementById("hello");
+  p.innerHTML+"Hello, World!" ;
+}
